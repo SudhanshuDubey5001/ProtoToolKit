eb6ec51f09d2d1730b18d5efff285ea55c5aaa83
Sudhanshu Dubey <31352594+SudhanshuDubey5001@users.noreply.github.com>
2025-08-10 19:58:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,22 +17,22 @@ export default defineConfig({
       : []),
   ],
   base: 'https://proto-tool-kit.vercel.app/',
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
-    },
-  },
-  root: path.resolve(import.meta.dirname, "client"),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(import.meta.dirname, "client", "src"),
+  //     "@shared": path.resolve(import.meta.dirname, "shared"),
+  //     "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+  //   },
+  // },
+  // root: path.resolve(import.meta.dirname, "client"),
+  // build: {
+  //   outDir: path.resolve(import.meta.dirname, "dist/public"),
+  //   emptyOutDir: true,
+  // },
+  // server: {
+  //   fs: {
+  //     strict: true,
+  //     deny: ["**/.*"],
+  //   },
+  // },
 });
